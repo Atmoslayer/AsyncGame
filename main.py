ad60225c9b6032e10637788796533ffd36fad275
Atmoslayer <ddsomdim@gmail.com>
2024-01-25 17:20:43 +0300

    Reorganize project structure

--- main.py
+++ main.py
@@ -1,12 +1,123 @@
+import asyncio
 import argparse
 import curses
 import os
 import random
 import time
 
-from animations import blink, fire, animate_rocket, fly_garbage, sleep
+from itertools import cycle
+
+from frames_control_functions import read_controls, draw_frame, get_frame_size, check_frame, update_speed
+
 
 TIC_TIMEOUT = 0.1
+
+
+async def sleep(tics=1):
+    for tick in range(tics):
+        await asyncio.sleep(0)
+
+
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
+async def animate_rocket(canvas, rocket_row, rocket_column, rocket_frames, max_row, max_column):
+    global coroutines
+    rocket_row_size, rocket_column_size = get_frame_size(rocket_frames[0])
+    row_speed = column_speed = 0
+    for rocket_frame in cycle(rocket_frames):
+        rows_direction, columns_direction, space_pressed = read_controls(canvas)
+        row_speed, column_speed = update_speed(row_speed, column_speed, rows_direction, columns_direction)
+
+        rocket_row += row_speed
+        rocket_column += column_speed
+
+        rocket_row = check_frame(rocket_row, rows_direction, max_row, rocket_row_size)
+        rocket_column = check_frame(rocket_column, columns_direction, max_column, rocket_column_size)
+
+        draw_frame(canvas, rocket_row, rocket_column, rocket_frame)
+
+        # if space_pressed:
+        #     coroutines.append(fire(canvas, rocket_row, rocket_column + 2))
+
+        await sleep(1)
+
+        draw_frame(canvas, rocket_row, rocket_column, rocket_frame, negative=True)
+
+
+async def blink(canvas, row, column, timeout, symbol='*'):
+
+    while True:
+
+        await sleep(timeout)
+
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await sleep(20)
+
+        canvas.addstr(row, column, symbol)
+        await sleep(3)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await sleep(5)
+
+        canvas.addstr(row, column, symbol)
+        await sleep(3)
+
+
+async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
+    """Animate garbage, flying from top to bottom. Сolumn position will stay same, as specified on start."""
+    rows_number, columns_number = canvas.getmaxyx()
+
+    column = max(column, 0)
+    column = min(column, columns_number - 1)
+
+    row = 0
+
+    while row < rows_number:
+        draw_frame(canvas, row, column, garbage_frame)
+        await asyncio.sleep(0)
+        draw_frame(canvas, row, column, garbage_frame, negative=True)
+        row += speed
+
+
+async def fill_orbit_with_garbage(canvas, max_column, garbage_quantity):
+
+    garbage_frames = [
+        open(os.path.join(f'{os.getcwd()}/templates/garbage', garbage_file), "r").read() for garbage_file
+        in os.listdir(f'{os.getcwd()}/templates/garbage')
+    ]
+
+    while True:
+        garbage_column = random.randint(1, max_column - 10)
+        garbage_frame = random.choice(garbage_frames)
+        garbage_timeout = random.randint(1, garbage_quantity)
+        coroutines.append(fly_garbage(canvas, column=garbage_column, garbage_frame=garbage_frame))
+        await sleep(garbage_timeout)
 
 
 def stars_generator(canvas, max_row, max_column):
@@ -58,21 +169,6 @@ def draw(canvas):
             canvas.refresh()
             canvas.border()
         time.sleep(TIC_TIMEOUT)
-
-
-async def fill_orbit_with_garbage(canvas, max_column, garbage_quantity):
-
-    garbage_frames = [
-        open(os.path.join(f'{os.getcwd()}/templates/garbage', garbage_file), "r").read() for garbage_file
-        in os.listdir(f'{os.getcwd()}/templates/garbage')
-    ]
-
-    while True:
-        garbage_column = random.randint(1, max_column - 10)
-        garbage_frame = random.choice(garbage_frames)
-        garbage_timeout = random.randint(1, garbage_quantity)
-        coroutines.append(fly_garbage(canvas, column=garbage_column, garbage_frame=garbage_frame))
-        await sleep(garbage_timeout)
 
 
 if __name__ == '__main__':
